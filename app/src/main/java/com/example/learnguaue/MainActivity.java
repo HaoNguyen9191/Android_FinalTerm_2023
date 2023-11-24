98f188e63d7ee153c878efca03863090fe68e1b1
Diep <tiendiep2003@gmail.com>
2023-11-24 16:29:26 +0700

    login,create cocab

--- app/src/main/java/com/example/learnguaue/MainActivity.java
+++ app/src/main/java/com/example/learnguaue/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.learnguaue;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private BottomNavigationView MnavigationView;
+//    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnItemReselectedListener(new BottomNavigationView.OnItemSelectedListener(){
+
+        } );
     }
 }
---
+++ app/src/main/java/com/example/learnguaue/main_screen_activity.java
@@ -0,0 +1,8 @@
+package com.example.learnguaue;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class main_screen_activity {
+}
